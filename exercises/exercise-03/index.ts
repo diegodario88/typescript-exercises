8cd463f9ebea930c4b1ab87583483466c2f92d05
Diego Dario <diegodario88@gmail.com>
2020-05-26 12:03:49 -0300

    solved: 3

--- exercises/exercise-03/index.ts
+++ exercises/exercise-03/index.ts
@@ -1,4 +1,4 @@
-import chalk from 'chalk';
+import chalk from 'chalk'
 
 /*
 
@@ -42,38 +42,37 @@ interface Admin {
 type Person = User | Admin;
 
 const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
-];
+  { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+  { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+  { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+  { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+]
 
-function isAdmin(person: Person) {
-    return person.type === 'admin';
+function isAdmin(person: User | Admin): person is Admin {
+  return (person as Admin).role !== undefined
 }
-
-function isUser(person: Person) {
-    return person.type === 'user';
+function isUser(person: User | Admin): person is User {
+  return (person as User).occupation !== undefined
 }
 
 function logPerson(person: Person) {
-    let additionalInformation: string = '';
-    if (isAdmin(person)) {
-        additionalInformation = person.role;
-    }
-    if (isUser(person)) {
-        additionalInformation = person.occupation;
-    }
-    console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
+  let additionalInformation = ''
+  if (isAdmin(person)) {
+    additionalInformation = person.role
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation
+  }
+  console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`)
 }
 
-console.log(chalk.yellow('Admins:'));
-persons.filter(isAdmin).forEach(logPerson);
+console.log(chalk.yellow('Admins:'))
+persons.filter(isAdmin).forEach(logPerson)
 
-console.log();
+console.log()
 
-console.log(chalk.yellow('Users:'));
-persons.filter(isUser).forEach(logPerson);
+console.log(chalk.yellow('Users:'))
+persons.filter(isUser).forEach(logPerson)
 
 // In case if you are stuck:
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html#using-type-predicates
