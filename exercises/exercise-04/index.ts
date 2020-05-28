8ffbfffc76f9130aa79b997f09b538dc33321bad
Diego Dario <diegodario88@gmail.com>
2020-05-27 21:39:48 -0300

    solved: 4

--- exercises/exercise-04/index.ts
+++ exercises/exercise-04/index.ts
@@ -1,4 +1,4 @@
-import chalk from 'chalk';
+import chalk from 'chalk'
 
 /*
 
@@ -48,70 +48,72 @@ interface Admin {
 type Person = User | Admin;
 
 const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    {
-        type: 'admin',
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        type: 'user',
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        type: 'admin',
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    },
-    {
-        type: 'user',
-        name: 'Wilson',
-        age: 23,
-        occupation: 'Ball'
-    },
-    {
-        type: 'admin',
-        name: 'Agent Smith',
-        age: 23,
-        role: 'Administrator'
-    }
-];
+  { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+  {
+    type: 'admin',
+    name: 'Jane Doe',
+    age: 32,
+    role: 'Administrator'
+  },
+  {
+    type: 'user',
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut'
+  },
+  {
+    type: 'admin',
+    name: 'Bruce Willis',
+    age: 64,
+    role: 'World saver'
+  },
+  {
+    type: 'user',
+    name: 'Wilson',
+    age: 23,
+    occupation: 'Ball'
+  },
+  {
+    type: 'admin',
+    name: 'Agent Smith',
+    age: 23,
+    role: 'Administrator'
+  }
+]
 
-const isAdmin = (person: Person): person is Admin => person.type === 'admin';
-const isUser = (person: Person): person is User => person.type === 'user';
+const isAdmin = (person: Person): person is Admin => person.type === 'admin'
+const isUser = (person: Person): person is User => person.type === 'user'
 
 function logPerson(person: Person) {
-    let additionalInformation: string = '';
-    if (isAdmin(person)) {
-        additionalInformation = person.role;
-    }
-    if (isUser(person)) {
-        additionalInformation = person.occupation;
-    }
-    console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
+  let additionalInformation = ''
+  if (isAdmin(person)) {
+    additionalInformation = person.role
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation
+  }
+  console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`)
 }
 
-function filterUsers(persons: Person[], criteria: User): User[] {
-    return persons.filter(isUser).filter((user) => {
-        let criteriaKeys = Object.keys(criteria) as (keyof User)[];
-        return criteriaKeys.every((fieldName) => {
-            return user[fieldName] === criteria[fieldName];
-        });
-    });
+type Criteria = Partial<Omit<User, 'type'>>;
+
+function filterUsers(persons: Person[], criteria: Criteria): User[] {
+  return persons.filter(isUser).filter((user) => {
+    const criteriaKeys = Object.keys(criteria) as (keyof Criteria) []
+    return criteriaKeys.every((fieldName) => {
+      return user[fieldName] === criteria[fieldName]
+    })
+  })
 }
 
-console.log(chalk.yellow('Users of age 23:'));
+console.log(chalk.yellow('Users of age 23:'))
 
 filterUsers(
-    persons,
-    {
-        age: 23
-    }
-).forEach(logPerson);
+  persons,
+  {
+    age: 23
+  }
+).forEach(logPerson)
 
 // In case if you are stuck:
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types
